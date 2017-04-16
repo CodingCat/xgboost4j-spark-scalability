47a14b1d15a64255425380f7eae1807bb748f1cc
CodingCat <zhunansjtu@gmail.com>
2017-04-16 15:21:36 -0700

    transform

--- src/main/scala/me/codingcat/xgboost4j/AirlineDataGenerator.scala
+++ src/main/scala/me/codingcat/xgboost4j/AirlineDataGenerator.scala
@@ -42,7 +42,8 @@ object AirlineDataGenerator {
       (depDelay: String) => if (depDelay.toInt >= 15) true else false).apply(col("DepDelay")))
     val extractedDF = dfWithNewCol.select("Month", "DayofMonth", "DayOfWeek", "DepTime",
       "UniqueCarrier", "Origin", "Dest", "Distance", "dep_delayed_15min")
-    val convertedDF = extractedDF.selectExpr("Month", "DayOfMonth", "DayOfWeek", "DepTime",
+    val convertedDF = extractedDF.selectExpr("Month", "DayOfMonth", "DayOfWeek",
+      "CAST(DepTime As Float) as DepTime",
       "UniqueCarrier", "Origin", "Dest", "CAST(Distance As Float) as Distance", "dep_delayed_15min")
     val sampledDF = convertedDF.sample(withReplacement = false, ratioRate)
     sampledDF.write.mode(SaveMode.Overwrite).parquet(outputDir)
