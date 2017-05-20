cb6147a2a8e099b38269820b11c95c18e0203af4
CodingCat <zhunansjtu@gmail.com>
2017-05-19 21:46:33 -0700

    use one hot encoder

--- src/main/scala/me/codingcat/xgboost4j/CriteoDataGenerator.scala
+++ src/main/scala/me/codingcat/xgboost4j/CriteoDataGenerator.scala
@@ -18,7 +18,7 @@
 package me.codingcat.xgboost4j
 
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer, VectorAssembler}
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 
@@ -31,11 +31,16 @@ object CriteoDataGenerator {
       stringIndexerArray(i) = new StringIndexer().setInputCol(s"category_$i").setOutputCol(
         s"category_$i" + "_index")
     }
+    val oneHotEncodersArray = new Array[OneHotEncoder](26)
+    for (i <- 0 until 26) {
+      oneHotEncodersArray(i) = new OneHotEncoder().setInputCol(s"category_$i" + "_index").
+        setOutputCol(s"category_$i" + "_encoded")
+    }
     val numericColumns = (0 until 13).map(i => s"numeric_$i").toArray
-    val categoryColumns = stringIndexerArray.map(_.getOutputCol)
+    val categoryColumns = oneHotEncodersArray.map(_.getOutputCol)
     val vectorAssembler = new VectorAssembler().setInputCols(numericColumns ++ categoryColumns).
       setOutputCol("features")
-    pipeline.setStages(stringIndexerArray :+ vectorAssembler)
+    pipeline.setStages(stringIndexerArray ++ oneHotEncodersArray :+ vectorAssembler)
   }
 
   def main(args: Array[String]): Unit = {
