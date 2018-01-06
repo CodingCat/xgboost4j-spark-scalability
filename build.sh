b3109c61e7b7d74ca097213318cc0ee2b6a0cd92
Nan Zhu <nanzhu@uber.com>
2018-01-06 15:01:14 -0800

    build tools for benchmark

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TARGET_URL=https://github.com/codingcat/xgboost.git
+TARGET_BRANCH=master2
+
+# compile xgboost
+rm -rf xgboost_build;
+git clone --recursive $TARGET_URL xgboost_build; 
+cd xgboost_build;
+
+if [ -n $TARGET_BRANCH ]; then
+  git fetch --all
+  git checkout $TARGET_BRANCH;
+fi
+
+rm -rf lib/*
+cd jvm-packages;
+mvn package;
+cp xgboost4j-spark/target/*-dependencies.jar ../../lib/
+
+# compile benchmark
+cd ../../;
+sbt assembly
+rm -rf xgboost_build
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,1 @@
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.3")
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
