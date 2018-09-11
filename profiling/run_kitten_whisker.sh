f4c69c6ceb24297e82d422d47f091bdb42b6a97c
Nan Zhu <nanzhu@uber.com>
2018-09-11 13:50:56 -0700

    adjust command relative path

--- profiling/run_kitten_whisker.sh
+++ profiling/run_kitten_whisker.sh
@@ -8,8 +8,7 @@ wget https://github.com/CodingCat/KittenWhisker/archive/release-0.1.tar.gz
 # uncompress tar ball
 tar -zxvf release-0.1.tar.gz
 
-# run the script to start xgboost4j-spark benchmark workload
-
+# run the script to compile KittenWhisker 
 cd KittenWhisker-release-0.1;
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -18,7 +17,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   . dev/build-docker.sh
 fi
 
+# update command to start benchmark workload
 cd -;
 cat > KittenWhisker-release-0.1/spark_app_cmd.sh <<'EOT'
-spark-submit --class me.codingcat.xgboost4j.AirlineClassifier --num-executors 10 --executor-memory 14g --executor-cores 8 --driver-memory 14g --driver-cores 4 --files conf/airline.conf --master yarn-cluster --queue hadoop-adhoc ../target/scala-2.11/xgboost4j-spark-scalability-assembly-0.1-SNAPSHOT.jar ./airline.conf
+spark-submit --class me.codingcat.xgboost4j.AirlineClassifier --num-executors 10 --executor-memory 14g --executor-cores 8 --driver-memory 14g --driver-cores 4 --files conf/airline.conf --master yarn-cluster --queue hadoop-adhoc target/scala-2.11/xgboost4j-spark-scalability-assembly-0.1-SNAPSHOT.jar ./airline.conf
 EOT
