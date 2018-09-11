8008b941f31e8dff84da2a1339d92668f5a7c627
Nan Zhu <nanzhu@uber.com>
2018-09-11 13:29:07 -0700

    command to start benchmark workload

--- profiling/run_kitten_whisker.sh
+++ profiling/run_kitten_whisker.sh
@@ -12,3 +12,7 @@ tar -zxvf release-0.1.tar.gz
 
 cd KittenWhisker-release-0.1;
 . dev/build-docker.sh
+cd -;
+cat > KittenWhisker-release-0.1/spark_app_cmd.sh <<'EOT'
+spark-submit --class me.codingcat.xgboost4j.AirlineClassifier --num-executors 10 --executor-memory 14g --executor-cores 8 --driver-memory 14g --driver-cores 4 --files conf/airline.conf --master yarn-cluster --queue hadoop-adhoc ../target/scala-2.11/xgboost4j-spark-scalability-assembly-0.1-SNAPSHOT.jar ./airline.conf
+EOT
