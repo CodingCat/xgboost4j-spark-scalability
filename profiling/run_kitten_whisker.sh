8679b41ef872b6500eb9ee3d84b5580f19988046
Nan Zhu <nanzhu@uber.com>
2018-09-11 13:42:34 -0700

    automatically determine whether to use docker to compile

--- profiling/run_kitten_whisker.sh
+++ profiling/run_kitten_whisker.sh
@@ -11,7 +11,13 @@ tar -zxvf release-0.1.tar.gz
 # run the script to start xgboost4j-spark benchmark workload
 
 cd KittenWhisker-release-0.1;
-. dev/build-docker.sh
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  mvn package 
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  . dev/build-docker.sh
+fi
+
 cd -;
 cat > KittenWhisker-release-0.1/spark_app_cmd.sh <<'EOT'
 spark-submit --class me.codingcat.xgboost4j.AirlineClassifier --num-executors 10 --executor-memory 14g --executor-cores 8 --driver-memory 14g --driver-cores 4 --files conf/airline.conf --master yarn-cluster --queue hadoop-adhoc ../target/scala-2.11/xgboost4j-spark-scalability-assembly-0.1-SNAPSHOT.jar ./airline.conf
