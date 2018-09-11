ae8d3f097657a631c70937a9fd0feb1dd074cec2
Nan Zhu <nanzhu@uber.com>
2018-09-11 14:47:56 -0700

    directly clone github repo

--- profiling/run_kitten_whisker.sh
+++ profiling/run_kitten_whisker.sh
@@ -9,16 +9,11 @@ PROJECT_DIR="$SCRIPT_DIR/../"
 
 cd $PROJECT_DIR
 
-if [ ! -d "KittenWhisker-release-0.1" ]; then
-  # delete leftovers
-  rm -rf release-0.1.tar.gz*
+if [ ! -d "KittenWhisker" ]; then
   # download kittenwhisker
-  wget https://github.com/CodingCat/KittenWhisker/archive/release-0.1.tar.gz
-  # uncompress tar ball
-  tar -zxvf release-0.1.tar.gz
-
+  git clone git@github.com:CodingCat/KittenWhisker.git
   # run the script to compile KittenWhisker 
-  cd KittenWhisker-release-0.1;
+  cd KittenWhisker
 
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
     mvn package 
@@ -29,6 +24,6 @@ fi
 
 # update command to start benchmark workload
 cd $PROJECT_DIR;
-cat > KittenWhisker-release-0.1/spark_app_cmd.sh <<'EOT'
+cat > KittenWhisker/spark_app_cmd.sh <<'EOT'
 spark-submit --class me.codingcat.xgboost4j.AirlineClassifier --num-executors 10 --executor-memory 14g --executor-cores 8 --driver-memory 14g --driver-cores 4 --files conf/airline.conf --master yarn-cluster --queue hadoop-adhoc target/scala-2.11/xgboost4j-spark-scalability-assembly-0.1-SNAPSHOT.jar ./airline.conf
 EOT
