6c27c6c829753c561715876b5cd6541c796cfaa8
Nan Zhu <nanzhu@uber.com>
2018-09-10 20:25:27 -0700

    profiling script

---
+++ profiling/run_kitten_whisker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+# download kittenwhisker
+wget https://github.com/CodingCat/KittenWhisker/archive/release-0.1.tar.gz
+
+# uncompress tar ball
+tar -zxvf release-0.1.tar.gz
+
+# run the script to start xgboost4j-spark benchmark workload
+
+
