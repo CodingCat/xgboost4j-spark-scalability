518032fbbd2ad97a5a32d446d46a64962fb0a4d1
Nan Zhu <nanzhu@uber.com>
2018-12-18 10:26:25 -0800

    better pure classifier

--- src/main/scala/me/codingcat/xgboost4j/PureClassifier.scala
+++ src/main/scala/me/codingcat/xgboost4j/PureClassifier.scala
@@ -17,6 +17,9 @@
 
 package me.codingcat.xgboost4j
 
+import java.io.File
+
+import com.typesafe.config.ConfigFactory
 import ml.dmlc.xgboost4j.scala.spark.XGBoostRegressor
 
 import org.apache.spark.sql.SparkSession
@@ -27,17 +30,21 @@ object PureClassifier {
     val featureCol = args(0)
     val labelCol = args(1)
     val inputPath = args(2)
+    val ratio = args(3).toDouble
+    val configFile = args(4)
+
+    val conf = ConfigFactory.parseFile(new File(configFile))
+    import scala.collection.JavaConverters._
+    val xgbParamMap = conf.entrySet().asScala.map {
+      entry =>
+        entry.getKey -> conf.getString(entry.getKey)
+    }.toMap
 
     val spark = SparkSession.builder().getOrCreate()
+    val trainingSet = spark.read.parquet(inputPath).select(featureCol, labelCol).sample(ratio)
 
-    val trainingSet = spark.read.parquet(inputPath).select(featureCol, labelCol)
+    val xgbRegressor = new XGBoostRegressor(xgbParamMap)
 
-    val xgbRegressor = new XGBoostRegressor()
-    xgbRegressor.setFeaturesCol(featureCol).setLabelCol(labelCol)
-    xgbRegressor.setMaxDepth(10)
-    xgbRegressor.setNumRound(500)
-    xgbRegressor.setNumWorkers(200)
-    xgbRegressor.setNthread(4)
     val startTS = System.currentTimeMillis()
     val xgbRegressionModel = xgbRegressor.fit(trainingSet)
     println(s"finished training in ${System.currentTimeMillis() - startTS}")
